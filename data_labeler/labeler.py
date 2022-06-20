6d54b0fbbe6429dc5668757a4ee147011742890d
David L <davidlobobellon@gmail.com>
2022-06-20 20:23:33 +0200

    New functionalities for the data labaler

--- data_labeler/labeler.py
+++ data_labeler/labeler.py
@@ -30,7 +30,41 @@ def read_object_tracking(path_to_file,threshold):
 
 
 
+
+def visualize_results(cap ,object_id ,tracker, video_name,file_name):
+    """Shows the detections for a specific object"""
+    import cv2
+    import time
+    appereances = tracker[object_id]
+    for k ,v in appereances.items():
+        cap.set(cv2.CAP_PROP_POS_FRAMES, k)
+        success, img = cap.read()
+        try:
+            cv2.rectangle(img ,(v[0] ,v[1]) ,(v[0 ] +v[2] ,v[1 ] +v[3]) ,(0 ,255 ,0) ,2)
+            cv2.imshow('Results ' +str(object_id) ,img)
+        except cv2.error:
+            pass
+        button = cv2.waitKey(3)
+        if (button == ord('l') or button == ord('r')):
+            frames = grabnext20(cap,appereances,k)
+            v_name =save_individual_video('data_labeler',video_name,k,'./data_labeler/selected_videos/',frames,(224,224))
+            save_info(file_name,video_name,button)
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+def visualize_all_results(cap ,tracker, video_name, file_name):
+    """Shows ALL the detections"""
+    import cv2
+    import time
+    for o in tracker.keys():
+        visualize_results(cap,o,tracker,video_name, file_name)
+
+
 def grabnext20(cap,appereances,frame):
+    """Given the detections for a specific object at a specific frame. Grab the next 20 frames where that object
+    appears"""
     frames = []
     counter = 0
     for f,c in appereances.items():
@@ -38,7 +72,8 @@ def grabnext20(cap,appereances,frame):
             if f > frame:
                 cap.set(cv2.CAP_PROP_POS_FRAMES,f )
                 success, img = cap.read()
-                frames.append(img[c[1]:c[1] + c[3],c[0] : c[0] + c[2]])
+                img = img[c[1]:c[1] + c[3],c[0] : c[0] + c[2]]
+                frames.append(pad_image((224,224),img))
                 counter+=1
         else:
             break
@@ -48,37 +83,82 @@ def grabnext20(cap,appereances,frame):
     return frames
 
 
-def visualize_results(cap ,object_id ,tracker):
-    """Shows the detections for a specific object"""
+
+
+
+def pad_image(desired_shape,img):
+    """Pads an image according to the desired shape we want for the video"""
+    import numpy as np
+    w = img.shape[1]
+    h = img.shape[0]
+    if w > desired_shape[1]:
+        img = img[:,:desired_shape[1]]
+        a1 = 0
+        a2 = 0
+    else:
+        a1 = round((desired_shape[1] - w) / 2)
+        a2 = desired_shape[1]-w-a1
+
+    if h > desired_shape[0]:
+        img = img[:desired_shape[0], :]
+        b1 = 0
+        b2 = 0
+    else:
+        b1 = round((desired_shape[0] - h) / 2)
+        b2 = desired_shape[0]-h-b1
+
+
+    padded_image = np.pad(img, ((b1,b2), (a1,a2), (0, 0)), 'constant')
+
+    return padded_image
+
+
+def save_individual_video(s3_folder,video_name,frame,local_path,video,desired_size):
+    """Saves the video locally and to s3
+    INPUTS:
+        - s3_folder: name of the s3 folder where videos should be stored
+        - object: the object id that will be included in the name
+        - local_path: Local path to folder where the videos would be stored  CRATE IT BEFORE RUNNING
+        - videos: array of shape (224,224,3)
+        - Length of the videos
+
+    OUTPUTS:
+        - video_names : LIST with the video names that will be later used to build the CSV file that contains path and label
+    """
     import cv2
-    import time
-    appereances = tracker[object_id]
-    for k ,v in appereances.items():
-        cap.set(cv2.CAP_PROP_POS_FRAMES, k)
-        success, img = cap.read()
-        cv2.rectangle(img ,(v[0] ,v[1]) ,(v[0 ] +v[2] ,v[1 ] +v[3]) ,(0 ,255 ,0) ,2)
-        cv2.imshow('Results ' +str(object_id) ,img)
-        cv2.waitKey(1)
-    cap.release()
+    import boto3
+    s3 = boto3.client('s3')
+    fps = 20
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')  # note the lower case
+    vout = cv2.VideoWriter()
+    key_name = video_name + '_' + str(frame) + '.mp4'
+    success = vout.open(local_path + key_name, fourcc, fps, desired_size, True)
+    for frame in range(len(video)):
+        vout.write(video[frame])
+    vout.release()
     cv2.destroyAllWindows()
+    s3_key = s3_folder + '/' + key_name
+    s3.upload_file(local_path + key_name, Bucket='thesis-videos-dlb', Key=s3_key)
+    return s3_key
 
 
 
-def visualize_all_results(cap ,tracker):
-    """Shows ALL the detections"""
-    import cv2
-    import time
-    for o in tracker.keys():
-        appereances = tracker[o]
-        for k ,v in appereances.items():
-            cap.set(cv2.CAP_PROP_POS_FRAMES, k)
-            success, img = cap.read()
-            cv2.rectangle(img ,(v[0] ,v[1]) ,(v[0 ] +v[2] ,v[1 ] +v[3]) ,(0 ,255 ,0) ,2)
-            cv2.imshow('Results' ,img)
-            cv2.waitKey(1)
-    cap.release()
-    cv2.destroyAllWindows()
-    time.sleep(10)
+
+def save_info(file_name,video_name,label):
+    """Writes the s3 path and the label in a txt file for later training purposes"""
+    try:
+        with open(file_name,'a') as file:
+            file.write(video_name)
+            file.write(',')
+            file.write(label)
+            file.write('\n')
+    except FileNotFoundError:
+        with open(file_name,'w') as file:
+            file.write(video_name)
+            file.write(',')
+            file.write(label)
+            file.write('\n')
+
 
 ##
 
